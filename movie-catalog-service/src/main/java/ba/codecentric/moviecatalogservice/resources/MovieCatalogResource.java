4999fb4fc966143cc89716d2a7d3eced812a6cc8
Sasa Ilic <sasa.ilic@codecentric.de>
2019-08-06 11:23:14 +0200

    #01 add rest controller in movie catalog services

---
+++ movie-catalog-service/src/main/java/ba/codecentric/moviecatalogservice/resources/MovieCatalogResource.java
@@ -0,0 +1,22 @@
+package ba.codecentric.moviecatalogservice.resources;
+
+import ba.codecentric.moviecatalogservice.models.CatalogItem;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collections;
+import java.util.List;
+
+@RestController
+@RequestMapping("/catalog")
+public class MovieCatalogResource {
+
+    @RequestMapping("/{userId}")
+    public List<CatalogItem> getCatalog(@PathVariable("userId")String userId) {
+
+        return Collections.singletonList(
+                new CatalogItem("Transformes", "test", 4)
+        );
+    }
+}
